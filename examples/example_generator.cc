706207c86241fbc820de7f8bbcd7976c29bba545
j4niwzis <j4niwzis@proton.me>
2026-02-26 12:30:36 +0000

    Update example_generator.cc

--- examples/example_generator.cc
+++ examples/example_generator.cc
@@ -4,22 +4,45 @@
 
 // Allocation free generators
 
+// clang-format off
+
 auto my_generator() {
-  return GENERATOR((int i), (.i = 0),
+  return GENERATOR((int i), (.i = 0), 
     YIELD(42);
     WHILE(i != 10)(
-      ++i;
       YIELD(i);
+      ++i;
     )
-    return rec<int, 16>{};
+    return rec<int, 16>();
   );
 }
 
+auto my_generator2() {
+  return GENERATOR_ES(
+    int i = 0; // You can use state inside a generator as well.
+    YIELD(42);
+    WHILE(i != 10)(
+      int i2 = 0;
+      ++i;
+      YIELD(i);
+      YIELD(i);
+      ++i2;
+      YIELD(i2);
+    )
+    return rec<int, 40>{};
+  );
+}
+// clang-format on
+
 int main() {
   auto gen = my_generator();
-  std::println("{}", gen); // prints [42, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  gen.i = 3;
+  std::println("{}", gen);  // prints [42, 3, 4, 5, 6, 7, 8, 9]
+  std::println("{}",
+               my_generator2());  // prints [42, 1, 1, 1, 2, 2, 1, 3, 3, 1, 4, 4, 1, 5, 5, 1, 6, 6, 1, 7, 7, 1, 8, 8, 1, 9, 9, 1, 10, 10, 1]
   /*
-  for(auto i : gen) {
+  auto gen2 = my_generator2();
+  for(auto i : gen2) {
     std::println("{}", i);
   }
   */
